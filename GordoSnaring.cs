8e4d10e35e3d14c8b2727844e5365e0cd6525652
Lucas W. <weidmlucas@gmail.com>
2026-02-27 20:12:29 -0300

    adding emerald slime

---
+++ GordoSnaring.cs
@@ -0,0 +1,28 @@
+﻿using EmeraldSlime;
+using HarmonyLib;
+using MonomiPark.SlimeRancher.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRoneTutorial
+{
+    [HarmonyPatch(typeof(GordoSnare))]
+    [HarmonyPatch("GetGordoIdForBait")]
+    class GordoSnaring
+    {
+        [HarmonyPriority(800)]
+        public static bool Prefix(GordoSnare __instance, ref Identifiable.Id __result)
+        {
+            if (__instance.GetPrivateField<SnareModel>("model").baitTypeId != Identifiable.Id.BEET_VEGGIE || Randoms.SHARED.GetInRange(0, 100) > 60)
+                return true;
+            else
+            {
+                __result = Ids.EMERALD_GORDO;
+                return false;
+            }
+        }
+    }
+}
--- main.cs
+++ main.cs
@@ -7,21 +7,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using static SRML.SR.AchievementRegistry;
 
-namespace KalistaSlime
+namespace EmeraldSlime
 {
     [EnumHolder]
     public static class Ids
     {
-        public static readonly Identifiable.Id KALISTA_SLIME;
-        public static readonly Identifiable.Id KALISTA_PLORT;
-        public static readonly PediaDirector.Id PEDIA_KALISTA_SLIME;
+        public static readonly Identifiable.Id EMERALD_SLIME;
+        public static readonly Identifiable.Id EMERALD_PLORT;
+        public static readonly Identifiable.Id EMERALD_GORDO;
+        public static readonly PediaDirector.Id PEDIA_EMERALD_SLIME;
     }
     public class Main : ModEntryPoint
     {
         public override void PreLoad()
         {
-            PlortCreation.PlortPreLoad(Ids.KALISTA_PLORT, "Kalista Plort", false);
-            SpawnCreation.CreateSingleZoneSpawner(Ids.KALISTA_SLIME, ZoneDirector.Zone.REEF, 0.1f);
+            PlortCreation.PlortPreLoad(Ids.EMERALD_PLORT, "Emerald Plort", false);
+            SpawnCreation.CreateSingleZoneSpawner(Ids.EMERALD_SLIME, ZoneDirector.Zone.DESERT, 0.02f);
             HarmonyInstance.PatchAll();
         }
 
@@ -32,14 +33,16 @@ namespace KalistaSlime
 
         public override void PostLoad()
         {
-            var KalistaPlort = PlortCreation.CreatePlort("Kalista Plort", Ids.KALISTA_PLORT, Vacuumable.Size.NORMAL, Color.cyan, Color.blue, Color.blue);
-            PlortCreation.PlortLoad(Ids.KALISTA_PLORT, 20f, 10f, KalistaPlort, TextureUtils.LoadImage("KALI1.jpg").CreateSprite(), new Color32(230, 120, 10, 255), true, true, false);
-            var KalistaSlime = SlimeCreation.SlimeBaseCreate(Ids.KALISTA_SLIME, "Kalista_Slime", "Kalista Slime", "slimeKalista", "Kalista_Slime", Identifiable.Id.BOOM_SLIME, Identifiable.Id.RAD_SLIME, Identifiable.Id.PINK_SLIME, Identifiable.Id.PINK_SLIME, SlimeEat.FoodGroup.GINGER, Identifiable.Id.GINGER_VEGGIE, Identifiable.Id.GLASS_SHARD_CRAFT, Identifiable.Id.BOMB_BALL_TOY, Ids.KALISTA_PLORT, false, TextureUtils.LoadImage("KALI2.png").CreateSprite(), Vacuumable.Size.NORMAL, true, 0f, 0.1f, Color.cyan, Color.blue, Color.green, Color.white, Color.cyan, Color.white, Color.cyan, Color.green, Color.black, Color.green, Color.green, Color.black, Color.white, Color.cyan);
-            SlimeCreation.LoadSlime(KalistaSlime);
-            SlimePediaCreation.LoadSlimePediaIcon(Ids.PEDIA_KALISTA_SLIME, TextureUtils.LoadImage("KALI2.png").CreateSprite());
-            SlimePediaCreation.PreLoadSlimePediaConnection(Ids.PEDIA_KALISTA_SLIME, Ids.KALISTA_SLIME, PediaRegistry.PediaCategory.SLIMES);
-            SlimePediaCreation.CreateSlimePediaForSlime(Ids.PEDIA_KALISTA_SLIME, "Kalista is the Spear of Vengeance.", "Gilded Ginger", "Gilded Ginger", "A specter of wrath and retribution, Kalista is the undying spirit of vengeance, an armored nightmare summoned from the Shadow Isles to hunt deceivers and traitors.The betrayed may cry out in blood to be avenged, but Kalista only answers those willing to pay with their very souls.Those who become the focus of Kalista's wrath should make their final peace, for any pact sealed with this grim hunter can only end with the cold, piercing fire of her soul-spears.", "No One, Kalista is Friendly :3", "Very valuable due to its scarcity.");
-            TranslationPatcher.AddPediaTranslation(t.pedia_kalista_slime, "Kalista Slime");
+            var EmeraldPlort = PlortCreation.CreatePlort("Emerald Plort", Ids.EMERALD_PLORT, Vacuumable.Size.NORMAL, new Color32(18, 95, 45, 255), new Color32(95, 255, 160, 255), new Color32(18, 95, 45, 255));
+            PlortCreation.PlortLoad(Ids.EMERALD_PLORT, 600f, 50f, EmeraldPlort, TextureUtils.LoadImage("EmeraldPlort.png").CreateSprite(), new Color32(95, 255, 160, 255), true, true, false);
+            var EmeraldSlime = SlimeCreation.SlimeBaseCreate(Ids.EMERALD_SLIME, "Emerald Slime", "Emerald Slime", "Emerald Slime", "Emerald Slime", Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.CRYSTAL_SLIME, Identifiable.Id.LUCKY_SLIME, SlimeEat.FoodGroup.GINGER, Identifiable.Id.GINGER_VEGGIE, Identifiable.Id.GINGER_VEGGIE, Identifiable.Id.TREASURE_CHEST_TOY, Ids.EMERALD_PLORT, false, TextureUtils.LoadImage("EmeraldSlime.png").CreateSprite(), Vacuumable.Size.NORMAL, false, 0f, 0.1f, new Color32(120, 255, 170, 255), new Color32(45, 200, 120, 255), new Color32(10, 95, 55, 255), new Color32(190, 255, 220, 255), new Color32(8, 70, 40, 255), new Color32(5, 45, 28, 255), new Color32(2, 25, 15, 255), new Color32(210, 255, 230, 255), new Color32(20, 120, 70, 255), new Color32(5, 20, 10, 255), new Color32(170, 255, 210, 255), new Color32(45, 200, 120, 255), new Color32(10, 95, 55, 255), new Color32(90, 255, 170, 255));
+            SlimeCreation.LoadSlime(EmeraldSlime);
+            SlimePediaCreation.LoadSlimePediaIcon(Ids.PEDIA_EMERALD_SLIME, TextureUtils.LoadImage("EmeraldSlime.png").CreateSprite());
+            SlimePediaCreation.PreLoadSlimePediaConnection(Ids.PEDIA_EMERALD_SLIME, Ids.EMERALD_SLIME, PediaRegistry.PediaCategory.SLIMES);
+            SlimePediaCreation.CreateSlimePediaForSlime(Ids.PEDIA_EMERALD_SLIME, "Emerald Slimes are crystalline slimes that glow softly as light passes through their gemlike bodies.", "Gilded Ginger", "Gilded Ginger", "Scientists believe Emerald Slimes form when a Quantum Slime remains exposed to ancient mineral deposits for long periods of time. The minerals slowly fuse with its shifting matter, stabilizing parts of its body into a semi-crystalline structure. Despite this stability, their internal energy continues to pulse and shimmer, giving them their characteristic glow. Ranchers report that Emerald Slimes are unusually calm when undisturbed, but will suddenly relocate when stressed, as if slipping through the environment rather than physically moving through it.", "Living near Emerald Slimes can be disorienting. Objects may occasionally appear displaced, and loose items often vanish only to reappear nearby moments later. Prolonged exposure has been known to cause dizziness in ranchers due to their intense light refraction. High walls help, but do not guarantee containment.", "Emerald Plorts contain tightly packed crystalline matrices capable of storing and releasing energy in small bursts. Engineers have begun experimenting with them as stabilizers in delicate devices and advanced ranch tech. However, improper handling may result in sudden energy discharge, leaving tools briefly disabled or scrambled.");
+            TranslationPatcher.AddPediaTranslation("t.pedia_emerald_slime", "Emerald Slime");
+            var EmeraldGordo = GordoCreation.CreateGordoWithIcon(Ids.EMERALD_GORDO, "Emerald Gordo", Identifiable.Id.CRYSTAL_GORDO, Ids.EMERALD_SLIME, Ids.EMERALD_SLIME, "Emerald", ZoneDirector.Zone.DESERT, "Emerald Gordo", Ids.EMERALD_SLIME, 30, TextureUtils.LoadImage("EmeraldGordo.png").CreateSprite());
+            SlimeCreation.LoadSlime(EmeraldGordo);
         }
     }
 }
